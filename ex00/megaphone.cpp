059e721629632d2f6737f906586da92239aee0d0
jeportie <jeportie@student.42.fr>
2025-03-16 00:38:10 +0000

    push pause

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -10,4 +10,10 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
 
+int	main(void)
+{
+	std::cout << "Hello World!" << std::endl;
+	return (0);
+}
