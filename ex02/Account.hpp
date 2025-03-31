960e88bc0d5c71f92bc57dcf104bb483553deb94
jeportie <jeportie@student.42.fr>
2025-03-31 13:20:47 +0000

    push pause

--- ex02/Account.hpp
+++ ex02/Account.hpp
@@ -1,6 +1,6 @@
 // ************************************************************************** //
 //                                                                            //
-//                Account.hpp for GlobalBanksters United                //
+//                Account.hpp for GlobalBanksters United				      //
 //                Created on  : Thu Nov 20 19:43:15 1989                      //
 //                Last update : Wed Jan 04 14:54:06 1992                      //
 //                Made by : Brad "Buddy" McLane <bm@gbu.com>                  //
@@ -16,47 +16,40 @@
 //                               Account Class                                //
 // ************************************************************************** //
 
-class Account {
-
-
+class Account
+{
 public:
+    typedef Account t;
 
-	typedef Account		t;
+    static int  getNbAccounts(void);
+    static int  getTotalAmount(void);
+    static int  getNbDeposits(void);
+    static int  getNbWithdrawals(void);
+    static void displayAccountsInfos(void);
 
-	static int	getNbAccounts( void );
-	static int	getTotalAmount( void );
-	static int	getNbDeposits( void );
-	static int	getNbWithdrawals( void );
-	static void	displayAccountsInfos( void );
+    Account(int initial_deposit);
+    ~Account(void);
 
-	Account( int initial_deposit );
-	~Account( void );
-
-	void	makeDeposit( int deposit );
-	bool	makeWithdrawal( int withdrawal );
-	int		checkAmount( void ) const;
-	void	displayStatus( void ) const;
-
+    void makeDeposit(int deposit);
+    bool makeWithdrawal(int withdrawal);
+    int  checkAmount(void) const;
+    void displayStatus(void) const;
 
 private:
+    static int _nbAccounts;
+    static int _totalAmount;
+    static int _totalNbDeposits;
+    static int _totalNbWithdrawals;
 
-	static int	_nbAccounts;
-	static int	_totalAmount;
-	static int	_totalNbDeposits;
-	static int	_totalNbWithdrawals;
+    static void _displayTimestamp(void);
 
-	static void	_displayTimestamp( void );
+    int _accountIndex;
+    int _amount;
+    int _nbDeposits;
+    int _nbWithdrawals;
 
-	int				_accountIndex;
-	int				_amount;
-	int				_nbDeposits;
-	int				_nbWithdrawals;
-
-	Account( void );
-
+    Account(void);
 };
-
-
 
 // ************************************************************************** //
 // vim: set ts=4 sw=4 tw=80 noexpandtab:                                      //
@@ -64,6 +57,5 @@ private:
 // -*- mode: c++-mode;                                                       -*-
 // -*- fill-column: 75; comment-column: 75;                                  -*-
 // ************************************************************************** //
-
 
 #endif /* __ACCOUNT_H__ */
