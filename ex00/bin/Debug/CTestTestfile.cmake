a8df7fee15b99b0f360c7c79bcade357b835acb4
jeportie <jeportie@student.42.fr>
2025-04-01 14:56:57 +0000

    push pause

---
+++ ex00/bin/Debug/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /root/projects/cpp_day_0/ex00
+# Build directory: /root/projects/cpp_day_0/ex00/bin/Debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("/root/projects/cpp_day_0/ex00/bin/Debug/test_megaphone[1]_include.cmake")
