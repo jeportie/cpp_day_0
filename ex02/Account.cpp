4612154015733e147060cef7c96c293d171f5928
jeportie <jeportie@student.42.fr>
2025-04-01 08:16:19 +0000

    push pause

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -13,42 +13,29 @@
 #include <iostream>
 #include "Account.hpp"
 
-static int _nbAccounts = 0;
-static int _totalAmount = 0;
-static int _totalNbDeposits = 0;
+static int _nbAccounts         = 0;
+static int _totalAmount        = 0;
+static int _totalNbDeposits    = 0;
 static int _totalNbWithdrawals = 0;
 
 Account::Account(int initial_deposit)
 {
-	std::cout << "Account constructor called" << std::endl;
-	Account::_displayTimestamp();
-	std::cout << "" << std::endl;
-	Account::makeDeposit(initial_deposit);
+    std::cout << "Account constructor called" << std::endl;
+    Account::_displayTimestamp();
+    std::cout	<< " index:" << Account::_accountIndex
+				<< ";amount:" << Account::_amount
+				<< ";created" << std::endl;
+    Account::makeDeposit(initial_deposit);
 }
 
-Account::~Account(void)
-{
-	std::cout << "Account destructor called" << std::endl;
-}
+Account::~Account(void) { std::cout << "Account destructor called" << std::endl; }
 
-int	Account::getNbAccounts(void)
-{
-	return (Account::_nbAccounts);
-} 
+int Account::getNbAccounts(void) { return (Account::_nbAccounts); }
 
-int  Account::getTotalAmount(void)
-{
-	return (Account::_totalAmount);
-}
+int Account::getTotalAmount(void) { return (Account::_totalAmount); }
 
-int  Account::getNbDeposits(void)
-{
-	return (Account::_totalNbDeposits);
-}
+int Account::getNbDeposits(void) { return (Account::_totalNbDeposits); }
 
-int  Account::getNbWithdrawals(void)
-{
-	return (Account::_totalNbWithdrawals);
-}
+int Account::getNbWithdrawals(void) { return (Account::_totalNbWithdrawals); }
 
 void displayAccountsInfos(void);
